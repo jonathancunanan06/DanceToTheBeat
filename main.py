293a77d8d2ce8ad4764a6b152024b356a9264d60
jonathancunanan06 <cunananjc100@gmail.com>
2025-01-15 19:57:28 +0800

    - partial responsiveness

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flaskr import create_app, socketio
 
 def main():
     app = create_app()
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
